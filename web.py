53f396404006cb323b1d2f6257c2f48054815016
Oded Lazar <odedlaz@gmail.com>
2016-02-21 17:38:49 +0200

    added say name

--- web.py
+++ web.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/say/<text>')
+def say(text):
+   return text
+
 if __name__ == '__main__':
     app.run(debug=True)
